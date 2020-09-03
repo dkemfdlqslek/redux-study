51de62380b874681ac22acb18609afb5820dd87f
LSK <dkemfdkqslek@gmail.com>
2020-09-03 16:46:04 +0900

    200903_설명 추가 및 redux_pattern_image insert

---
+++ src/assets/images/index.js
@@ -0,0 +1,3 @@
+export const redux_pattern_lg = require('./diagrams/redux_pattern_lg.png');
+export const redux_pattern_md = require('./diagrams/redux_pattern_md.png');
+export const redux_pattern_sm = require('./diagrams/redux_pattern_sm.png');
--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import CounterApp from 'components/counter';
 import 'styles/_CounterPage.scss';
+import {
+    redux_pattern_sm,
+    redux_pattern_md,
+    redux_pattern_lg
+} from 'assets/images';
+
+const ReduxPatternImg = () => {
+   const imageSrcSet = `${redux_pattern_sm} 300w,
+                        ${redux_pattern_md} 500w,
+                        ${redux_pattern_lg} 761w`;
+    return(
+        <img 
+        srcSet={imageSrcSet}
+        sizes='(max-width: 767px) 300px,
+                (max-width: 1023px) 500px,
+                761px'
+        src={redux_pattern_lg}
+        alt="redux pattern"/>
+    )
+}
 
 const Counter = () => {
+
+    
 
 
     return(
@@ -20,9 +42,15 @@ const Counter = () => {
                         also, you can adjust raise or down amount to modify the number in input text-box.
                         and to click reset button, your counter will reset to zero.
                     </p>
-                    <p>redux</p>                   
+                    <p>
+                        In redux-system, every request changing state is called "action".
+                        "action" means literally "interaction" between user and application.
+                        -click the button, go to another page, typing input text-box, something else.
+                    </p>
+                    <ReduxPatternImg />              
                 </article>
             </section>
+            
         </React.Fragment>
     );
 }
